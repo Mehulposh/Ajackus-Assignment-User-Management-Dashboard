db6357fd4ca69ab641c3e08593b2cac6e7f06187
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-23 21:19:34 +0530

    added random age and department

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button  from '../Button/Button'
 import { deleteUser } from '../../api/apiCalls'
 
+const departments = ["HR", "Engineering", "Sales", "Marketing"];
 
 const UserCard = ({user,setEditId,onDelete}) => {
     const handleEdit = (id) => {
@@ -21,13 +22,19 @@ const UserCard = ({user,setEditId,onDelete}) => {
             
         }
     }
+
+    // 🔹 Pick random department if none exists
+    const randomDepartment = user.department || departments[Math.floor(Math.random() * departments.length)];
+    const randomAge = user.age || Math.floor(Math.random() * (60 - 20 + 1)) + 20;
+
+
   return (
     <div>
         <div className={`w-[350px] p-3 rounded bg-gray-300`}>
         <h2>Name: {user.name}</h2>
-        <p>Email: {user.email}</p>
-        <p>Age: {user.age}</p>
-        <p>Department:</p>
+        <p>Email: {user.email}</p>  
+        <p>Age: {user.age || randomAge}</p>
+        <p>Department: {user.department || randomDepartment}</p>
         
         <div className='space-x-3 mt-3'>
             <Button 
